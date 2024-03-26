cbeafc18d89213166bca85c31a5fca49ebf3515b
Gaurav Jha <59960697+Jhajeen01@users.noreply.github.com>
2024-03-26 08:39:13 +0000

    Update page1.tf

--- page1.tf
+++ page1.tf
@@ -1,4 +1,9 @@
-resource "azurerm_resource_group" "rg" {
-  name = "abc"
-  location = "Central India"
+data "azurerm_resource_group" "rg" {
+  name = "Gaurav_RG"
+}
+resource "azurerm_virtual_network" "vnet" {
+  name = "vnetad"
+  address_space = [ "10.0.0.0/25" ]
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location = data.azurerm_resource_group.rg.location
 }
